ec4c531776da4aa3d5e1638277ed5a4e6ccb7524
KoStard <rub099999720@gmail.com>
2019-10-27 15:45:47 +0400

    Static workers and design: - adding static workers to allow offline access to the application - showing message about available newer version - added some footer information + showing that the script can be accessed in offline mode - fixed firefox compatibility

--- docs/main.js
+++ docs/main.js
@@ -1,31 +1,101 @@
-{
-    const box = document.getElementById('box');
-    const boxInner = box.querySelector('.inner');
-    boxInner.ondragenter = function (event) {
-        box.classList.add('drag-drop');
+let updateNotifier;
+let newWorker;
+
+function updateOfflineAccessibilityMessageStatus(accessible=true) {
+  if (accessible) {
+    document.getElementsByClassName('offline-status-message')[0].classList.add('show');
+  } else {
+    document.getElementsByClassName('offline-status-message')[0].classList.remove('show');
+  }
+}
+
+function setupDOMEvents() {
+  updateNotifier = document.getElementById('update_notifier');
+
+  updateNotifier.querySelector('.update').addEventListener('click', function () {
+    if (newWorker) {
+      newWorker.postMessage({
+        action: 'skipWaiting'
+      });
     }
-    boxInner.ondragleave = function (event) {
-        box.classList.remove('drag-drop');
+  });
+
+  const box = document.getElementById('box');
+  const fileInput = document.getElementById('file-input');
+  fileInput.ondragenter = function (event) {
+    box.classList.add('drag-drop');
+  }
+  fileInput.ondragleave = function (event) {
+    box.classList.remove('drag-drop');
+  }
+  fileInput.onchange = async function (event) {
+    if (fileInput.files[0]) {
+      while (!processFile) {
+        await new Promise(function (resolve) {
+          setTimeout(resolve, 250);
+        });
+      }
+      box.classList.remove('drag-drop');
+      let initialValue = fileInput.value;
+      let resp = await processFile(fileInput.files[0]);
+      if (fileInput.value == initialValue) {
+        fileInput.value = null;
+      }
+      if (!resp) {
+        box.classList.add('invalid');
+        setTimeout(() => {
+          box.classList.remove('invalid');
+        }, 500);
+      }
     }
-    boxInner.onchange = async function (event) {
-        if (boxInner.files[0]) {
-            while (!processFile) {
-                await new Promise(function (resolve) {
-                    setTimeout(resolve, 250);
-                });
+  }
+}
+
+function setupSW() {
+  if ('serviceWorker' in navigator) {
+    // Adding service worker - will allow the app to run and load in offline mode
+    // Show in the DOM that the program can run in offline mode
+    let base = document.location.pathname.split('/')[1];
+    navigator.serviceWorker.register(`/${base}/sw.js`, { scope: `/${base}/` }).then(function (reg) {
+
+      if (reg.installing) {
+        console.log('Service worker installing');
+      } else if (reg.waiting) {
+        console.log('Service worker installed');
+      } else if (reg.active) {
+        console.log('Service worker active');
+      }
+
+      updateOfflineAccessibilityMessageStatus(true);
+
+      reg.addEventListener('updatefound', function () {
+        newWorker = reg.installing;
+        newWorker.addEventListener('statechange', function () {
+          if (newWorker.state == 'installed') {
+            if (navigator.serviceWorker.controller) {
+              updateNotifier.classList.add('show');
             }
-            box.classList.remove('drag-drop');
-            let initialValue = boxInner.value;
-            let resp = await processFile(boxInner.files[0]);
-            if (boxInner.value == initialValue) {
-                boxInner.value = null;
-            }
-            if (!resp) {
-                box.classList.add('invalid');
-                setTimeout(() => {
-                    box.classList.remove('invalid');
-                }, 500);
-            }
-        }
-    }
+          }
+        })
+      })
+
+    }).catch(function (error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+
+    let refreshing = false;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+      if (refreshing) {
+        return;
+      }
+      window.location.reload();
+      refreshing = true;
+    })
+  }
+}
+
+window.onload = function () {
+  setupDOMEvents();
+  setupSW();
 }
---
+++ docs/sw.js
@@ -0,0 +1,60 @@
+const VERSION = 'v0.0.1';
+const LOCAL = true; // This has to be true to run on localhost
+const BASEPATH = self.registration.scope;
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(VERSION).then(function (cache) {
+      return cache.addAll([
+        BASEPATH,
+        BASEPATH + 'index.html',
+        BASEPATH + 'styles.css',
+        BASEPATH + 'main.js',
+        BASEPATH + 'ansi2unicode-web.js',
+        BASEPATH + 'ansi2unicode_web.wasm',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+        'https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.207/distr/fira_code.css',
+        'https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.207/distr/woff2/FiraCode-Bold.woff2',
+        'https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.207/distr/woff2/FiraCode-Medium.woff2',
+        'https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.207/distr/woff2/FiraCode-Regular.woff2',
+      ]);
+    })
+  );
+});
+
+// Removing old caches
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key != VERSION) {
+        return caches.delete(key);
+      }
+    }));
+  }))
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request); // Can add these to the cache too
+    }
+  }));
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action == 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+
+
+/**
+ * Some websites for service workers:
+ * - https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
+ *
+ */
